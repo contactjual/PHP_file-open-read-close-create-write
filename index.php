f2ed1af53bab1ff21b8c67ba8924fda16f08ff34
Jual Rana <contactjual@gmail.com>
2025-01-02 19:23:44 +0600

    write/create donew

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // file open, read, close
 
-$my_file = fopen("file.text", "r") or die("Sorry this path no found");
+// $my_file = fopen("file.text", "r") or die("Sorry this path no found");
 // echo fread ($my_file, filesize("file.text")); // full file
 // echo fgets ($my_file, filesize("file.text")); // first line
 // echo fgetc ($my_file); // first char
@@ -10,16 +10,25 @@ $my_file = fopen("file.text", "r") or die("Sorry this path no found");
 
 // file end of file function 
 
-while(!feof($my_file)) {
-    echo fgetc ($my_file)."<br>"; // single char golo dekhabe
-}
+// while(!feof($my_file)) {
+//     echo fgetc ($my_file)."<br>"; // single char golo dekhabe
+// }
 
 
-fclose($my_file);
-
-
+// fclose($my_file);
 
 
 
 
-?>f
+// fwrite() means file write........ 
+
+$file_container = fopen("write.text", "a") or die("Sorry cannot load");
+$my_writing = "Hi, I am written by php file_writing";
+fwrite($file_container, $my_writing);
+echo $my_writing;
+fclose($file_container);
+
+
+
+
+?>
